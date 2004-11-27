a004dea42e05b865ee36a8004171a06fc1801a1a
Mirco Bauer <meebey@meebey.net>
2004-11-27 23:01:40 +0000

    - added lots of comments

--- examples/test/test.cs
+++ examples/test/test.cs
@@ -28,16 +28,27 @@ using System.Collections;
 using System.Threading;
 using Meebey.SmartIrc4net;
 
+// This is an VERY basic example how your IRC application could be written
+// its mainly for showing how to use the API, this program just connects sends
+// a few message to a channel and waits for commands on the console
+// (raw RFC commands though! it's later explained).
+// There are also a few commands the IRC bot/client allows via private message.
 public class Test
 {
+    // make an instance of the high-level API
     public static IrcClient irc = new IrcClient();
 
+    // this method we will use to analyse queries (also known as private messages)
     public static void OnQueryMessage(object sender, IrcEventArgs e)
     {
         switch (e.Data.MessageArray[0]) {
+            // debug stuff
             case "dump_channel":
                 string requested_channel = e.Data.MessageArray[1];
+                // getting the channel (via channel sync feature)
                 Channel channel = irc.GetChannel(requested_channel);
+                
+                // here we send messages
                 irc.SendMessage(SendType.Message, e.Data.Nick, "<channel '"+requested_channel+"'>");
 
                 irc.SendMessage(SendType.Message, e.Data.Nick, "Name: '"+channel.Name+"'");
@@ -46,6 +57,8 @@ public class Test
                 irc.SendMessage(SendType.Message, e.Data.Nick, "Key: '"+channel.Key+"'");
                 irc.SendMessage(SendType.Message, e.Data.Nick, "UserLimit: '"+channel.UserLimit+"'");
 
+                // here we go through all users of the channel and show their
+                // hashtable key and nickname 
                 string nickname_list = "";
                 nickname_list += "Users: ";
                 IDictionaryEnumerator it = channel.Users.GetEnumerator();
@@ -58,14 +71,15 @@ public class Test
 
                 irc.SendMessage(SendType.Message, e.Data.Nick, "</channel>");
             break;
+            case "gc":
+                GC.Collect();
+            break;
+            // typical commands
             case "join":
                 irc.RfcJoin(e.Data.MessageArray[1]);
             break;
             case "part":
                 irc.RfcPart(e.Data.MessageArray[1]);
-            break;
-            case "gc":
-                GC.Collect();
             break;
             case "die":
                 Exit();
@@ -73,12 +87,14 @@ public class Test
         }
     }
 
+    // this method handles when we receive "ERROR" from the IRC server
     public static void OnError(object sender, ErrorEventArgs e)
     {
         System.Console.WriteLine("Error: "+e.ErrorMessage);
         Exit();
     }
     
+    // this method will get all IRC messages
     public static void OnRawMessage(object sender, IrcEventArgs e)
     {
         System.Console.WriteLine("Received: "+e.Data.RawMessage);
@@ -86,46 +102,79 @@ public class Test
     
     public static void Main(string[] args)
     {
-        System.Threading.Thread.CurrentThread.Name = "Main";
+        Thread.CurrentThread.Name = "Main";
+        
+        // wait time between messages, we can set this lower on own irc servers
         irc.SendDelay = 200;
+        
+        // we use channel sync, means we can use irc.GetChannel() and so on
         irc.ActiveChannelSyncing = true;
+        
+        // here we connect the events of the API to our written methods
+        // most have own event handler types, because they ship different data
         irc.OnQueryMessage += new IrcEventHandler(OnQueryMessage);
         irc.OnError += new ErrorEventHandler(OnError);
         irc.OnRawMessage += new IrcEventHandler(OnRawMessage);
 
         string[] serverlist;
+        // the server we want to connect to, could be also a simple string
         serverlist = new string[] {"irc.fu-berlin.de"};
         int port = 6667;
         string channel = "#smartirc";
         try {
+            // here we try to connect to the server and exceptions get handled
             irc.Connect(serverlist, port);
         } catch (ConnectionException e) {
+            // something went wrong, the reason will be shown
             System.Console.WriteLine("couldn't connect! Reason: "+e.Message);
             Exit();
         }
         
         try {
+            // here we logon and register our nickname and so on 
             irc.Login("SmartIRC", "SmartIrc4net Test Bot");
+            // join the channel
             irc.RfcJoin(channel);
             for (int i = 0; i < 3; i++) {
+                // here we send just 3 different types of messages, 3 times for
+                // testing the delay and flood protection (messagebuffer work)
                 irc.SendMessage(SendType.Message, channel, "test message "+i.ToString());
                 irc.SendMessage(SendType.Action, channel, "thinks this is cool "+i.ToString());
                 irc.SendMessage(SendType.Notice, channel, "SmartIrc4net rocks "+i.ToString());
             }
             
-            new Thread(new ThreadStart(ReadCommand)).Start();
+            // spawn a new thread to read the stdin of the console, this we use
+            // for reading IRC commands from the keyboard while the IRC connection
+            // stays in its own thread
+            new Thread(new ThreadStart(ReadCommands)).Start();
+            
+            // here we tell the IRC API to go into a receive mode, all events
+            // will be triggered by _this_ thread (main thread in this case)
+            // Listen() blocks by default, you can also use ListenOnce() if you
+            // need that does one IRC operation and then returns, so you need then 
+            // an own loop 
             irc.Listen();
+            
+            // when Listen() returns our IRC session is over, to be sure we call
+            // disconnect manually
             irc.Disconnect();
         } catch (ConnectionException) {
+            // this exception is handled becaused Disconnect() can throw a not
+            // connected exception
             Exit();
         } catch (Exception e) {
-            System.Console.WriteLine("Error occurred! Reason: "+e.Message);
+            // this should not happen by just in case we handle it nicely
+            System.Console.WriteLine("Error occurred! Message: "+e.Message);
+            System.Console.WriteLine("Exception: "+e.StackTrace);
             Exit();
         }
     }
     
-    public static void ReadCommand()
+    public static void ReadCommands()
     {
+        // here we read the commands from the stdin and send it to the IRC API
+        // WARNING, it uses WriteLine() means you need to enter RFC commands
+        // like "JOIN #test" and then "PRIVMSG #test :hello to you"
         while (true) {
             irc.WriteLine(System.Console.ReadLine());
         }
@@ -133,6 +182,7 @@ public class Test
     
     public static void Exit()
     {
+        // we are done, lets exit...
         System.Console.WriteLine("Exiting...");
         System.Environment.Exit(0);
     }
