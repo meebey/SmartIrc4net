248804fc4fbcfab802b7b50928bf318076b97621
Mirco Bauer <meebey@meebey.net>
2005-06-09 17:06:22 +0000

    - this should not be here (ServerPingEventArgs)

--- src/IrcConnection/Delegates.cs
+++ src/IrcConnection/Delegates.cs
@@ -31,5 +31,4 @@ namespace Meebey.SmartIrc4net
     public delegate void ReadLineEventHandler(object sender, ReadLineEventArgs e);
     public delegate void WriteLineEventHandler(object sender, WriteLineEventArgs e);
     public delegate void AutoConnectErrorEventHandler(object sender, AutoConnectErrorEventArgs e);
-    public delegate void ServerPingEventHandler(object sender, ServerPingEventArgs e);
 }
